811b10c94b12df93482b004d50ae23deca560dc1
Asphähyre <luka.boulagnon@epitech.eu>
2016-07-25 13:24:23 +0200

    Added is_open_extended's method

--- opening_hours/OpeningHours.py
+++ opening_hours/OpeningHours.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from ctypes import cdll, c_char_p, c_voidp
+from ctypes import cdll, c_char_p, c_voidp, c_int
 
 opening_hours = cdll.LoadLibrary("libopening-hours.so")
 
@@ -10,9 +10,15 @@ build.restype = c_voidp
 print_oh = opening_hours.print_oh
 print_oh.restype = c_char_p
 
+isopen = opening_hours.is_open_expended
+isopen.restype = c_int
+
 class OpeningHours:
     def __init__(self, expression):
         self.oh = build(c_char_p(expression))
 
     def __str__(self):
         return str(print_oh(c_voidp(self.oh)))
+
+    def is_open_expended(self, min, hour, day, month, year, day_of_week, day_of_year):
+        return isopen(c_voidp(self.oh), c_int(min), c_int(hour), c_int(day), c_int(month), c_int(year), c_int(day_of_week), c_int(day_of_year))
